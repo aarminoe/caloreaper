85a8b7a8a36f458c8aab723aa37e2af06d22d969
aarminoe <aarminoe@gmail.com>
2023-04-13 18:05:04 -0500

    app now connects to elastic beanstalk backend

---
+++ flask-server/application.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+application = Flask(__name__)
+CORS(application)
+
+@application.route("/")
+def home():
+    return "Hello!"
+
+@application.route("/members")
+def members():
+    return {"Members": ["member1", "member2"]}
+
+if __name__ == "__main__":
+    application.run(debug=True)
